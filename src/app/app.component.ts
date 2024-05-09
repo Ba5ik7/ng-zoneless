4975a6d03bab16cbabd801c75c6b4ec4f3958504
Wesley DuSell <dusellfam@gmail.com>
2024-05-09 00:04:38 -0400

    Zoneless

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,75 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { combineLatest, interval, map, tap } from 'rxjs';
+
+interface Geo {
+  lat: string;
+  lng: string;
+}
+
+interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, CommonModule],
+  template: `
+    <h1>{{ title }}</h1>
+    <router-outlet></router-outlet>
+    @if (viewModel$ | async; as viewModel) {
+    <h2>Users</h2>
+    <ul>
+      @for (user of viewModel.users; track $index) {
+      <li>{{ user.name }}</li>
+      <ul>
+        @if(viewModel.interval) {
+          <li>{{ user.email }}</li>
+        }
+      </ul>
+      }
+    </ul>
+    }
+  `,
+  styles: [``],
 })
 export class AppComponent {
   title = 'zoneless';
+
+  httpClient = inject(HttpClient);
+
+  viewModel$ = combineLatest({
+    users: this.fetchUsers(),
+    interval: interval(1000).pipe(map((value) => value % 2 === 0)),
+  }).pipe(tap(console.log));
+
+  fetchUsers() {
+    return this.httpClient.get<User[]>(
+      'https://jsonplaceholder.typicode.com/users'
+    );
+  }
 }
